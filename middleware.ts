f2ab48077e7289da480791704a1f518354a8e4ad
SeanB <sean.babayev@gmail.com>
2025-04-01 23:23:52 +0200

    Aktivera autentiseringskontroll i middleware

--- middleware.ts
+++ middleware.ts
@@ -3,17 +3,18 @@ import { withAuth } from "next-auth/middleware"
 // Middleware kommer att köras på alla routes som matchar detta mönster
 export default withAuth({
   callbacks: {
-    authorized: ({ token }) => true // Tillåt alla requests
+    authorized: ({ token }) => !!token // Kräv att användaren är inloggad
   },
 })
 
 // Konfigurera vilka sidor som kräver autentisering
 export const config = {
   matcher: [
-    // "/dashboard/:path*",
-    // "/add-favourite/:path*",
-    // "/api/contacts/:path*",
-    // "/transfer/:path*",
-    // "/transfer"
+    "/dashboard/:path*",
+    "/dashboard",
+    "/add-favourite/:path*",
+    "/api/contacts/:path*",
+    "/transfer/:path*",
+    "/transfer"
   ]
 }
